601ab6b07a58499dc5834d956476b5f92911aa79
sj <sj@DESKTOP-6F4NEMJ>
2023-07-25 22:27:38 +0900

    first commit

---
+++ LeeSeungJun/java_ws_04_4/src/com/ssafy/ws/step4/Restaurant.java
@@ -0,0 +1,8 @@
+package com.ssafy.ws.step4;
+
+/**
+ * 맛집 정보를 나타내는 클래스
+ */
+public class Restaurant {
+	//코드를 작성해주세요. 
+}
---
+++ LeeSeungJun/java_ws_04_4/src/com/ssafy/ws/step4/RestaurantTest.java
@@ -0,0 +1,10 @@
+package com.ssafy.ws.step4;
+
+/**
+ * 맛집 객체를 생성하고 정보를 출력하는 클래스
+ */
+public class RestaurantTest {
+	public static void main(String[] args) {
+		//코드를 작성해주세요. 
+	}
+}
