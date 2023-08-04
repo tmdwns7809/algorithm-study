f603cf2535fa195d1c40726ac3ee4a1eb9276512
이승준 <tmdwns7800@naver.com>
2023-08-04 17:57:17 +0900

    added

--- LeeSeungJun/algorithm_homework/src/boj_2023/Main.java
+++ LeeSeungJun/algorithm_homework/src/boj_2023/Main.java
@@ -1,29 +1,45 @@
 package boj_2023;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
 public class Main {
 
 	static int N;
+	static StringBuilder sb = new StringBuilder();
 	
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		N = sc.nextInt();
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		N = Integer.parseInt(br.readLine());
 		
-		
+		int[] n1s = {2,3,5,7};
+		for (int i =0; i < 4; i++) {
+			Solve(1, n1s[i]);
+		}
+		System.out.println(sb);
 	}
 	
-	static void Per(int cnt, int num, StringBuilder sb) {
+	static void Solve(int cnt, int num) {
 		if (cnt == N) {
 			sb.append(num).append('\n');
 			return;
 		}
-		
-		if (cnt == 0) {
-			for (int i = 1; i < 9; i++) {
+
+		for (int i = 1; i < 10; i+=2) {
+			int j = 3;
+			int newNum = num*10 + i;
+			while (true) {
+				if (newNum%j == 0) {
+					break;
+				}
+				
+				if (j > newNum/j) {
+					Solve(cnt+1, newNum);
+					break;
+				}
+				
+				j+=2;
 			}
-		} else {
-			
 		}
 	}
 }
---
+++ LeeSeungJun/algorithm_homework/src/swea_1222.java
@@ -0,0 +1,8 @@
+
+public class swea_1222 {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
