a3aca48aa83044e75b6d4b45a36d7e4262e3598d
이승준 <tmdwns7800@naver.com>
2023-08-08 17:54:38 +0900

    added

--- LeeSeungJun/swea_solution/src/week3/swea_1228/Solution.java
+++ LeeSeungJun/swea_solution/src/week3/swea_1228/Solution.java
@@ -3,7 +3,7 @@ package week3.swea_1228;
 import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.StringTokenizer;
 
@@ -15,7 +15,7 @@ public class Solution {
 		
 		for (int t = 1; t <= 10; t++) {
 			int N = Integer.parseInt(br.readLine());
-			List<Integer> arr = new ArrayList<Integer>();
+			List<Integer> arr = new LinkedList<Integer>();
 			StringTokenizer st = new StringTokenizer(br.readLine());
 			for (int i = 0; i < N; i++) {
 				if (i > 9) {
@@ -32,7 +32,7 @@ public class Solution {
 				int n = Integer.parseInt(st.nextToken());;
 				for (int j = idx; j < n + idx; j++) {
 					if (j < arr.size()) {
-						arr.set(j, Integer.parseInt(st.nextToken()));
+						arr.add(j, Integer.parseInt(st.nextToken()));
 					} else {
 						arr.add(Integer.parseInt(st.nextToken()));
 					}
---
+++ LeeSeungJun/swea_solution/src/week3/swea_4008/Solution.java
@@ -0,0 +1,10 @@
+package week3.swea_4008;
+
+public class Solution {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
